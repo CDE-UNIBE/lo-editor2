5c1978c29296fc22698cc668203008e03cc7d041
webrian <adrian.weber@cde.unibe.ch>
2014-01-20 11:54:00 +0100

    Minor fixes

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ rm -rfv $filename
 mkdir -v lo_editor2
 cp -v `find src/ | grep \.py$` lo_editor2/
 cp -v "src/lo-logo.png" lo_editor2/
+cp -v "src/metadata.txt" lo_editor2/
 zip -9rv $filename lo_editor2/
 rm -rf lo_editor2/
